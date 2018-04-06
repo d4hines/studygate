6d9edf38350fed18c358a474f8b0d556a1cd4e73
Daniel Hines <d4hines@gmail.com>
2018-04-06 09:04:38 -0400

    data flowing to CRM

--- src/main/fulcro_todomvc/api.clj
+++ src/main/fulcro_todomvc/api.clj
@@ -1,13 +1,15 @@
 (ns fulcro-todomvc.api
-  (:require [fulcro.server :as om]
+  (:require [clojure.core :refer [pr-str]]
             [datomic.api :as d]
+            [dynamics-clj.core :as dyn]
             [fulcro.datomic.protocols :as db]
-            [fulcro.server :refer [defmutation defquery-root]]
             [fulcro.logging :as log]
-            [dynamics-clj.core :as dyn]))
+            [fulcro.server :refer [defmutation defquery-root]]))
 
 (defonce last-id (atom 1000))
 (defonce requests (atom {}))
+(def todolists "new_todolistses")
+(def todoitems "new_todoitems")
 
 ; You can use a fully-qualified symbol with defmutation, and it will honor it. You cannot intern it though.
 ; This is special. Support viewer defines the client side of this. We have to define how the server receives it.
@@ -30,49 +32,60 @@
    {}
    fulcroids->tempids))
 
-(defn make-list-
-  "Make a new list with the given title on the given Datomic database connection.
+;; (defn make-list-
+  ;; "Make a new list with the given title on the given Datomic database connection.
 
-  Returns the real ID of the new list."
-  [connection list-name]
-  (let [;; id      (d/tempid :db.part/user)
-        record      {:new_title list-name}
-        ;; idmap   (:tempids @(d/transact connection tx))
-        real-id (dyn/create todolists record)
-        ]
-    real-id))
+  ;; Returns the real ID of the new list."
+  ;; [connection list-name]
+  ;; (let [;; id      (d/tempid :db.part/user)
+  ;;       record      {:new_title list-name}
+  ;;       ;; idmap   (:tempids @(d/transact connection tx))
+  ;;       real-id (dyn/create-record  todolists record)
+  ;;       ]
+  ;;   real-id))
 
 (defn make-list
   "Make a new list with the given title on the given Datomic database connection.
 
   Returns the real ID of the new list."
-  [connection list-name]
+  [config connection list-name]
   (let [id      (d/tempid :db.part/user)
         tx      [{:db/id id :list/title list-name}]
         idmap   (:tempids @(d/transact connection tx))
+        ;; Check to see if the list already exists in CRM, otherwise, create it.
+        crm-id (if-let [id (->> (str "new_title eq '" list-name "'")
+                                (dyn/retrieve-multiple config todolists nil)
+                                first
+                                :new_todolistsid)]
+                 (do (log/info "Existing List " list-name " found in CRM")
+                     id)
+                 (do (log/info "Creating List " list-name " in CRM")
+                     (dyn/create-record config todolists {:new_title list-name})))
         real-id (d/resolve-tempid (d/db connection) idmap id)
-        _ (log/info real-id)]
+        ;; e.g. 17592186045433
+        _ (log/info "Datomic ID" real-id)
+    _ (log/info "CRM ID" real-id)]
     real-id))
 
-(defn find-list-
-  "Find or create a list with the given name. Always returns a valid list ID."
-  [conn list-name]
-  (if-let [eid (-> (dyn/retrieve-multiple todolists ["new_todolistsid"] (str "new_title eq '" list-name "'"))
-                   first
-                   :new_todolistsid)]
-    eid
-    (make-list conn list-name)))
+;; (defn find-list-
+;;   "Find or create a list with the given name. Always returns a valid list ID."
+;;   [conn list-name]
+;;   (if-let [eid (-> (dyn/retrieve-multiple todolists ["new_todolistsid"] (str "new_title eq '" list-name "'"))
+;;                    first
+;;                    :new_todolistsid)]
+;;     eid
+;;     (make-list conn list-name)))
 
 (defn find-list
   "Find or create a list with the given name. Always returns a valid list ID."
-  [conn list-name]
+  [config conn list-name]
   (if-let [eid (d/q '[:find ?e . :in $ ?n :where [?e :list/title ?n]] (d/db conn) list-name)]
-    (do (log/info eid) eid)
-    (make-list conn list-name)))
+    (do (log/info "!!!!find-list EID" (pr-str eid)) eid)
+    (make-list config conn list-name)))
 
 (defmutation todo-new-item
   [{:keys [id text list-id]}]
-  (action [{:keys [todo-database]}]
+  (action [{:keys [todo-database ] {{:keys [crm-config]} :value} :config}]
           (let [connection         (db/get-connection todo-database) ; See fulcro-datomic for this API
                 datomic-id         (d/tempid :db.part/user)       ; in order to create an entity, we need a proper datomic temp ID
                 fulcroid->tempid   {id datomic-id}                ; remember that the incoming temp id (id) maps to the datomic one
@@ -82,6 +95,7 @@
                 tempid->realid     (:tempids result)              ; remap the incoming om tempid to the now-real datomic ID
                 fulcroids->realids (resolve-ids (d/db connection) fulcroid->tempid tempid->realid)]
             (log/info "Added list item " text " to " list)
+            ;; e.g. {#fulcro/tempid["99a1e4a0-eb4f-46b3-8bc9-bfc2aa974cab"] 17592186045437}
             (log/info fulcroids->realids)
             {:tempids fulcroids->realids})))
 
@@ -147,23 +161,31 @@
             (log/info "Deleted all cleared items in list " list-id)
             true)))
 
+(defn read-list [config connection query nm]
+  (let [list-id (find-list config connection nm)
+        db      (d/db connection)
+        rv      (d/pull db query list-id)
+        ;; {:db/id 17592186045431, :list/title "successList"}
+        _       (log/info (pr-str rv))
+        crm-id (:new_todolistsid
+                (first (dyn/retrieve-multiple config todolists nil
+                                              (str "new_title eq '" nm "'"))))
+        crm-todoitems (dyn/retrieve-multiple config todoitems
+                                         ["new_complete" "new_label" "new_todoitemid"]
+                                         (str "_new_todolistid_value eq " crm-id))] ; Datomic's pull can handle Fulcro query syntax
+    (assoc rv :new_todolistsid crm-id :crm_todoitems crm-todoitems)))
+
+(defquery-root :todos
+  "Returns the todo items for the given list."
+  (value [{:keys [query todo-database] {{:keys [crm-config]} :value} :config} {:keys [list]}]
+         (log/info "Responding to request for list: " list)
+         (let [connection (db/get-connection todo-database)]
+           (read-list crm-config connection query list))))
+
 (defn ensure-integer [n]
   (cond
     (string? n) (Integer/parseInt n)
     :else n))
-
-(defn read-list [connection query nm]
-  (let [list-id (find-list connection nm)
-        db      (d/db connection)
-        rv      (d/pull db query list-id)] ; Datomic's pull can handle Fulcro query syntax
-    rv))
-
-(defquery-root :todos
-  "Returns the todo items for the given list."
-  (value [{:keys [query todo-database]} {:keys [list]}]
-         (log/info "Responding to request for list: " list)
-         (let [connection (db/get-connection todo-database)]
-           (read-list connection query list))))
 
 (defquery-root :support-request
   "Get a support request by server ID (see server logs (NOT CLIENT Tx ID). This is required for the support viewer
--- src/main/fulcro_todomvc/server.clj
+++ src/main/fulcro_todomvc/server.clj
@@ -1,17 +1,39 @@
 (ns fulcro-todomvc.server
-  (:require
-    [fulcro.easy-server :as easy]
-    [fulcro.server :as server]
-    fulcro-todomvc.api                                      ; ensure the mutations and queries are loaded
-    [fulcro.datomic.core :refer [build-database]]))
+  (:require [clojure.pprint :refer [pprint]]
+            [com.stuartsierra.component :as component]
+            [fulcro.datomic.core :refer [build-database]]
+            [fulcro.easy-server :as easy]
+            [fulcro.server :as server]))
+
+(defrecord CRMClient [config]
+  ;; Implement the Lifecycle protocol
+  component/Lifecycle
+
+  (start [component*]
+    (println ";; Starting CRM API Client")
+    ;; In the 'start' method, initialize this component
+    ;; and start it running. For example, connect to a
+    ;; database, create thread pools, or initialize shared
+    ;; state.
+    (pprint component*)
+    component*)
+
+  (stop [component*]
+    (println ";; Stopping CRM API Client")
+    ;; In the 'stop' method, shut down the running
+    ;; component and release any external resources it has
+    ;; acquired.
+    ;; Return the component, optionally modified. Remember that if you
+    ;; dissoc one of a record's base fields, you get a plain map.
+component*))
 
 (defn make-system
   ([] (make-system "config/dev.edn"))
   ([config-path]
    (let [config-path config-path]                           ; in production, should use a filesystem file
      (easy/make-fulcro-server
-       :config-path config-path
-       :parser (server/fulcro-parser)                       ; allows us to use built-in multimethods and helper macros for reads/mutates
-       :parser-injections #{:todo-database}                 ; places the named components into the env of mutations and queries
-       :components {:todo-database (build-database :todo)   ; adds components to the server
-                    :logger        {}}))))
+      :config-path config-path
+      :parser (server/fulcro-parser)                       ; allows us to use built-in multimethods and helper macros for reads/mutates
+      :parser-injections #{:todo-database :config}                 ; places the named components into the env of mutations and queries
+      :components {:todo-database (build-database :todo)   ; adds components to the server
+                   :logger        {}}))))
